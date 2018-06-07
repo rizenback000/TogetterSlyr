414febcb5bb26fc0456621c48d19bc68b2462633
rizenback000 <10810847+rizenback000@users.noreply.github.com>
2018-06-07 19:55:47 +0900

    1.5.0へ更新
    -
    ModalWindow内から無理やりNinjaManagerの関数を呼んでいた部分をイベント発火に切り替えた(あまりいい形だとは思えない)
    - getTweetBox関数を追加
    - ModalWindowの一部メンバ変数にsetter, getterを追加
    -
    Togetterの仕様変更によりulの分割が無くなったのでloadPagesの読み込み部分を最適化
    - 一部わかりにくい変数名を変更

--- TogetterSlyr.user.js
+++ TogetterSlyr.user.js
@@ -2,7 +2,7 @@
 // @name        TogetterSlyr
 // @namespace   https://github.com/rizenback000/TogetterSlyr
 // @include     https://togetter.com/li/*
-// @version     1.4.0
+// @version     1.5.0
 // @description togetterのニンジャスレイヤーまとめを読みやすくする
 // @author      rizenback000
 // @require     https://rawgit.com/tuupola/jquery_lazyload/2.x/lazyload.js
@@ -63,20 +63,32 @@
     }
 
     /**
-     * gettwList - 全ツイートリストの取得
+     * getTweetBox - 全ツイートリストの取得(親を含む)
      *
-     * @param  {boolean} clone trueでコピーを作成
+     * @param  {Element==} body  description
+     * @return {Element=}       description
+     */
+    getTweetBox(clone, body) {
+      if (typeof body === 'undefined') body = document;
+      return body.querySelector('.tweet_box');
+    }
+
+
+    /**
+     * getTweetList - 全ツイートリストの取得(親を含まない)
+     *
+     * @param  {boolean=} clone trueでコピーを作成
      * @param  {Element=} body 探索するElementを指定する
      * @param  {string=} selector セレクター
      * @return {nodeList|Element[]}  cloneがfalseならnodeList、trueならElement配列
      */
-    gettwList(clone, body, selector) {
+    getTweetList(clone, body, selector) {
       if (typeof clone === 'undefined') clone = false;
       if (typeof body === 'undefined') body = document;
       if (typeof selector === 'undefined') selector = '.list_box.type_tweet';
 
       const tweets = body.querySelectorAll(selector);
-      // console.log('gettwList = '+selector);
+      // console.log('getTweetList = '+selector);
       // console.log(tweets);
       if (clone) {
         const arr = [];
@@ -94,8 +106,8 @@
      * @return {Element}      ページネーション
      */
     getPagination(body) {
-      if (typeof body === 'undefined') body = document;
-      return body.querySelector('.tweet_box .pagenation');
+      if (typeof body === 'undefined') body = this.getTweetBox();
+      return body.querySelector('.pagenation');
     }
 
 
@@ -172,7 +184,7 @@
     getUserIconList(clone, filter) {
       if (typeof clone === 'undefined') clone = false;
       if (typeof filter === 'undefined') filter = '';
-      return this.gettwList(clone, document, '.user_link img' + filter);
+      return this.getTweetList(clone, document, '.user_link img' + filter);
     }
 
 
@@ -193,20 +205,17 @@
       xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
-            const resTweets = xhr.response.querySelectorAll('.tweet_box[itemprop=articleBody]');
-            const resPage = self.getNowPage(xhr.response);
 
-            Array.from(resTweets, (twBox) => {
-              const twList = twBox.querySelectorAll('.list_box.type_tweet');
-              const twFragment = document.createDocumentFragment();
-              Array.from(twList, (tweet) => {
-                tweet.dataset.acqpage = resPage;
-                tweet.dataset.acqurl = xhr.response.URL;
-                twFragment.appendChild(tweet);
-              });
-              // ページネーションの直後に追加(twBoxの末尾に追加)
-              pagination.parentNode.insertBefore(twFragment, pagination);
+            const resPage = self.getNowPage(xhr.response);
+            const twList = self.getTweetList(false, xhr.response);
+            const twFragment = document.createDocumentFragment();
+            Array.from(twList, (tweet) => {
+              tweet.dataset.acqpage = resPage;
+              tweet.dataset.acqurl = xhr.response.URL;
+              twFragment.appendChild(tweet);
             });
+            // ページネーションの直後に追加(twBoxの末尾に追加)
+            pagination.parentNode.insertBefore(twFragment, pagination);
             maxPage--;
             // console.log('complete maxPage=' + maxPage + ' url=' + tgtUrl);
 
@@ -229,9 +238,9 @@
             } else {
               self.addEventNinja();
               if( self.getMaxPage() <= resPage ){
-                self.setStatusText('全てのページ読み込みを完了しました');
+                self.setStatusText('最後のページまで読み込みを完了しました');
               }else{
-                self.setStatusText(resPage+'まで読み込み完了');
+                self.setStatusText(resPage+'ページまで読み込み完了しました');
               }
             }
           }
@@ -257,57 +266,58 @@
    * 参考: https://syncer.jp/jquery-modal-window
    */
   class ModalWindow {
+
     constructor() {
       const self = this;
 
-      this.lazy = null;
+      this.lazy_ = null;
 
       this.windowScrollXPos_ = 0;
       this.windowScrollYPos_ = 0;
 
-      this.modalOverlay_ = document.createElement('div');
-      this.modalContents_ = document.createElement('div');
-      this.modalHeader_ = document.createElement('div');
-      this.modalContentsMain_ = document.createElement('div');
+      this.modalOverlay = document.createElement('div');
+      this.modalContents = document.createElement('div');
+      this.modalHeader = document.createElement('div');
+      this.modalContentsMain = document.createElement('div');
 
-      this.modalContents_.id = 'modalContent';
-      this.modalHeader_.id = 'modalHeader';
-      this.modalContentsMain_.id = 'modalContentsMain';
-      this.modalOverlay_.id = 'modelOverlay';
+      this.modalContents.id = 'modalContent';
+      this.modalHeader.id = 'modalHeader';
+      this.modalContentsMain.id = 'modalContentsMain';
+      this.modalOverlay.id = 'modelOverlay';
 
-      this.modalContents_.style.width = '50%';
-      this.modalContents_.style.height = '80%';
-      this.modalContents_.style.margin = '1.5em auto 0';
-      this.modalContents_.style.padding = '5px 20px 10px';
-      this.modalContents_.style.paddingTop = '0 px';
-      this.modalContents_.style.border = '2px solid #aaa';
-      this.modalContents_.style.zIndex = '2';
-      this.modalContents_.style.position = 'fixed';
-      this.modalContents_.style.backgroundColor = '#fff';
-      this.modalContents_.style.overflow = 'hidden';
-      this.modalContents_.style.display = 'none';
-      this.modalContents_.style.flexDirection = 'column';
+      this.modalContents.style.width = '50%';
+      this.modalContents.style.height = '80%';
+      this.modalContents.style.margin = '1.5em auto 0';
+      this.modalContents.style.padding = '5px 20px 10px';
+      this.modalContents.style.paddingTop = '0 px';
+      this.modalContents.style.border = '2px solid #aaa';
+      this.modalContents.style.zIndex = '2';
+      this.modalContents.style.position = 'fixed';
+      this.modalContents.style.backgroundColor = '#fff';
+      this.modalContents.style.overflow = 'hidden';
+      this.modalContents.style.display = 'none';
+      this.modalContents.style.flexDirection = 'column';
 
-      this.modalHeader_.style.borderBottom = '1px solid #aaa';
-      this.modalContentsMain_.style.overflowY = 'auto';
+      this.modalHeader.style.borderBottom = '1px solid #aaa';
+      this.modalContentsMain.style.overflowY = 'auto';
 
-      this.modalOverlay_.style.zIndex = '2';
-      this.modalOverlay_.style.display = 'none';
-      this.modalOverlay_.style.position = 'fixed';
-      this.modalOverlay_.style.top = '0';
-      this.modalOverlay_.style.left = '0';
-      this.modalOverlay_.style.width = '100%';
-      this.modalOverlay_.style.height = '120%';
-      this.modalOverlay_.style.backgroundColor = 'rgba(0,0,0,0.750)';
+      this.modalOverlay.style.zIndex = '2';
+      this.modalOverlay.style.display = 'none';
+      this.modalOverlay.style.position = 'fixed';
+      this.modalOverlay.style.top = '0';
+      this.modalOverlay.style.left = '0';
+      this.modalOverlay.style.width = '100%';
+      this.modalOverlay.style.height = '120%';
+      this.modalOverlay.style.backgroundColor = 'rgba(0,0,0,0.750)';
 
-      this.modalOverlay_.addEventListener('click', function(e) {
+      this.modalOverlay.addEventListener('click', function(e) {
         self.hide();
       });
 
-      document.body.appendChild(this.modalOverlay_);
-      document.body.appendChild(this.modalContents_);
-      this.modalContents_.appendChild(this.modalHeader_);
-      this.modalContents_.appendChild(this.modalContentsMain_);
+      document.body.appendChild(this.modalOverlay);
+      document.body.appendChild(this.modalContents);
+      this.modalContents.appendChild(this.modalHeader);
+      this.modalContents.appendChild(this.modalContentsMain);
 
       let queue = null;
       window.addEventListener('resize', function() {
@@ -316,14 +326,45 @@
       });
     }
 
+    get modalContents(){
+      return this._modalContents;
+    }
+
+    set modalContents(elem){
+      this._modalContents = elem;
+    }
+
+    get modalContentsMain(){
+      return this._modalContentsMain;
+    }
+
+    set modalContentsMain(elem){
+      this._modalContentsMain = elem;
+    }
+
+    get modalHeader(){
+      return this._modalHeader;
+    }
+
+    set modalHeader(elem){
+      this._modalHeader = elem;
+    }
+
+    get modalOverlay(){
+      return this._modalOverlay;
+    }
+
+    set modalOverlay(elem){
+      this._modalOverlay = elem;
+    }
 
     /**
      * show - モーダルを表示する
      *
-     * @param  {Element} baseTweet リアクションツイートのベースとなるツイート
+     * @param  {Element} callerTweet リアクションツイートのベースとなるツイート
      * @return {void}
      */
-    show(baseTweet) {
+    show(callerTweet) {
       // console.log('show');
 
       // 現在のスクロール位置を記録
@@ -333,39 +374,18 @@
       this.windowScrollYPos_ = dElm.scrollTop || dBody.scrollTop; // 現在位置のY座標
 
       this.hide();
-      const twBox = document.createElement('div');
-      const twFragment = document.createDocumentFragment();
-      const tweetsArr = NinjaManager.getReactTweets(baseTweet);
+      this.modalHeader.innerHTML = '';
+      this.modalContentsMain.innerHTML = '';
 
-      tweetsArr.forEach(function(item) {
-        twFragment.appendChild(item);
-      });
-      twBox.appendChild(twFragment);
+      const eve = new CustomEvent('modalShow', {detail: callerTweet});
+      eve.initEvent('modalShow', true, false);
+      document.body.dispatchEvent(eve);
 
-      const divHeader = document.createElement('div');
-      divHeader.appendChild(baseTweet.cloneNode(true));
-      this.modalHeader_.innerHTML = '';
-      if (typeof baseTweet.dataset.acqpage !== 'undefined') {
-        const dispPage = document.createElement('p');
-        dispPage.style.textAlign = 'center';
-        dispPage.textContent = '現在表示中のページ:';
-        const a = document.createElement('a');
-        a.style.fontWeight = 'bold';
-        a.href = baseTweet.dataset.acqurl;
-        a.textContent = baseTweet.dataset.acqpage;
-        dispPage.appendChild(a);
-        this.modalHeader_.appendChild(dispPage);
-      }
-      this.modalHeader_.append(divHeader);
-
-      this.modalContentsMain_.innerHTML = '';
-      this.modalContentsMain_.appendChild(twBox);
-
-      this.modalContents_.style.display = 'flex';
-      this.modalOverlay_.style.display = 'block';
+      this.modalContents.style.display = 'flex';
+      this.modalOverlay.style.display = 'block';
       this.centeringModalSyncer();
 
-      this.modalContentsMain_.scrollTop = 0;
+      this.modalContentsMain.scrollTop = 0;
       this.lazySet();
     }
 
@@ -376,8 +396,8 @@
      * @return {void}
      */
     hide() {
-      this.modalContents_.style.display = 'none';
-      this.modalOverlay_.style.display = 'none';
+      this.modalContents.style.display = 'none';
+      this.modalOverlay.style.display = 'none';
       this.lazyDestroy();
 
       // スクロール位置を移動
@@ -393,13 +413,13 @@
     centeringModalSyncer() {
       const w = window.document.documentElement.clientWidth;
       const h = window.document.documentElement.clientHeight;
-      const cw = this.modalContents_.offsetWidth;
-      const ch = this.modalContents_.offsetHeight;
+      const cw = this.modalContents.offsetWidth;
+      const ch = this.modalContents.offsetHeight;
       const pxleft = ((w - cw) / 2);
       const pxtop = ((h - ch) / 2);
 
-      this.modalContents_.style.left = pxleft + 'px';
-      this.modalContents_.style.top = pxtop + 'px';
+      this.modalContents.style.left = pxleft + 'px';
+      this.modalContents.style.top = pxtop + 'px';
     }
 
 
@@ -417,7 +437,7 @@
         src: 'data-lazy-src',
       };
       const images = document.querySelectorAll('#modalContent .lazy');
-      this.lazy = lazyload(images, opt);
+      this.lazy_ = lazyload(images, opt);
     }
 
 
@@ -429,9 +449,12 @@
     lazyDestroy() {
       // console.log('lazyDestroy');
       // 本当に効いてるかどうかわからんけどやらんよりマシ？
-      if (this.lazy !== null) this.lazy.destroy();
+      if (this.lazy_ !== null) this.lazy_.destroy();
     }
   }
+
+
+
 
 
   /**
@@ -442,17 +465,21 @@
       super();
       const self = this;
 
+
       // 実況まとめなのかを判断する
-      const tweets = this.gettwList();
+      const tweets = this.getTweetList();
       const nijaSoul = [].slice.call(tweets).some((tweet) => NinjaManager.isNinja(tweet));
       if (nijaSoul === false) return;
 
       // リアクション表示モーダルウィンドウ
       this.reactModal_ = new ModalWindow();
+
+
       // シームレスロードで次に読み込むURL
       this.seamlessNextUrl_ = '';
       // ステータステキスト
       this.statusDiv_ = document.createElement('div');
+      this.statusDiv_.style.fontWeight = 'bold';
       // ページ読み込みボタン
       this.loadBtn_ = document.createElement('button');
       this.loadBtn_.textContent = 'このページからまとめ読み開始';
@@ -464,13 +491,45 @@
         document.getElementsByClassName('contents_main')[0].style.display = 'none';
         self.loadPages(0);
         document.getElementsByClassName('contents_main')[0].style.display = 'block';
-        self.setStatusText('スクロールしていくと自動的に次のページを読み込みます(AutoPagerizeのようなアドオンなどは無効にしてください)');
+        self.setStatusText('◆TogetterSlyr◆<br>スクロールしていくと自動的に次のページを読み込みます'+
+        '<br>(AutoPagerizeのようなアドオンなどは無効にしてください)');
       });
       document.getElementsByClassName('title_box')[0].append(this.loadBtn_);
       document.getElementsByClassName('title_box')[0].append(this.statusDiv_);
 
       // シームレスロード
       window.onscroll = () => self.seamlessLoad();
+
+      // 本当にこんな実装でいいのか全然わからん
+      document.addEventListener('modalShow', function(e){
+        const twBox = document.createElement('div');
+        const twFragment = document.createDocumentFragment();
+        const twNinja = e.detail;
+        const tweetsArr = self.getReactTweets(twNinja);
+
+        tweetsArr.forEach(function(item) {
+          twFragment.appendChild(item);
+        });
+        twBox.appendChild(twFragment);
+
+        const ninjaTweet = document.createElement('div');
+        ninjaTweet.appendChild(twNinja.cloneNode(true));
+        if (typeof twNinja.dataset.acqpage !== 'undefined') {
+          const dispPage = document.createElement('p');
+          dispPage.style.textAlign = 'center';
+          dispPage.textContent = '現在表示中のページ:';
+          const a = document.createElement('a');
+          a.style.fontWeight = 'bold';
+          a.href = twNinja.dataset.acqurl;
+          a.textContent = twNinja.dataset.acqpage;
+          dispPage.appendChild(a);
+          self.reactModal_.modalHeader.appendChild(dispPage);
+        }
+        self.reactModal_.modalHeader.appendChild(ninjaTweet);
+        self.reactModal_.modalContentsMain.appendChild(twBox);
+      });
+
+
     }
 
 
@@ -485,17 +544,17 @@
       if (typeof clone === 'undefined') clone = false;
       if (typeof body === 'undefined') body = document;
       // 非表示になっていないツイートは公式のツイートとみなす
-      return this.gettwList(clone, body, '.list_box.type_tweet:not([style*=display])');
+      return this.getTweetList(clone, body, '.list_box.type_tweet:not([style*=display])');
     }
 
     /**
      * setStatusText - ステータステキストの設定
      *
-     * @param  {string} text 任意のステータステキスト
+     * @param  {string} html 任意のステータステキスト
      * @return {void}
      */
-    setStatusText(text) {
-      this.statusDiv_.textContent = text;
+    setStatusText(html) {
+      this.statusDiv_.innerHTML = html;
     }
 
 
@@ -580,8 +639,9 @@
      * @param  {Element} officialTweet 反応ツイートの基準となる公式ツイート
      * @return {Element[]}               配列に格納された反応ツイート
      */
-    static getReactTweets(officialTweet) {
+    getReactTweets(officialTweet) {
       // console.log('getReactTweets');
+      const self = this;
       const reactTweets = [];
       let ninjaFlg = false;
       let nextTweet = officialTweet.nextElementSibling;
@@ -593,24 +653,19 @@
         const cloneTweet = nextTweet.cloneNode(true);
         cloneTweet.style.display = 'block';
         reactTweets.push(cloneTweet);
-        // liの最後まで到達したらnullが返ってくるので次のulに進む
-        // togetter側の仕様変更により不要になった？
-        // if (nextTweet.nextElementSibling === null) {
-        //   nextTweet = nextTweet.parentElement.nextElementSibling;
-        //   if (nextTweet.tagName !== 'UL') break;
-        //   nextTweet = nextTweet.children[0];
-        // } else {
-        //   nextTweet = nextTweet.nextElementSibling;
-        // }
         nextTweet = nextTweet.nextElementSibling;
       }
       // 公式ツイートが出てこないまま終わった場合、次のツイートの取りこぼしがある可能性を示唆
       if (!ninjaFlg){
-        const divWarn = document.createElement('div');
-        divWarn.style.fontWeight = 'bold';
-        divWarn.innerHTML = '※TogetterSlyerからのお知らせ※<br>次のページにも実況ツイートが残っています。<br>'+
-        'もし取りこぼした実況ツイートも確認したい場合、次の公式ツイートを読み込んでから再度表示させてください。';
-        reactTweets.push(divWarn);
+        const loadedPage = parseInt(self.getTweetBox().lastElementChild.previousElementSibling.dataset.acqpage);
+        // 読み込み済み
+        if ( self.getMaxPage() > loadedPage ){
+          const divWarn = document.createElement('div');
+          divWarn.style.fontWeight = 'bold';
+          divWarn.innerHTML = '◆TogetterSlyer◆<br>次のページにも実況ツイートが残っています。<br>'+
+          'もし取りこぼした実況ツイートも確認したい場合、次の公式ツイートを読み込んでから再度表示させてください。';
+          reactTweets.push(divWarn);
+        }
       }
       return reactTweets;
     }
@@ -629,4 +684,6 @@
   }
 
   const nsm = new NinjaManager();
+
+
 })();
