b221353e9143ae86bbdec9e1b63ddd6cb751b5f7
rizenback000 <10810847+rizenback000@users.noreply.github.com>
2018-12-27 04:22:12 +0900

    バージョン変更忘れ

--- TogetterSlyr.user.js
+++ TogetterSlyr.user.js
@@ -2,7 +2,7 @@
 // @name        TogetterSlyr
 // @namespace   https://github.com/rizenback000/TogetterSlyr
 // @include     https://togetter.com/li/*
-// @version     1.7.3
+// @version     1.7.4
 // @description togetterのニンジャスレイヤーまとめを読みやすくする
 // @author      rizenback000
 // @require     https://rawgit.com/tuupola/jquery_lazyload/2.x/lazyload.js
